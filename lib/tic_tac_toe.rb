05c7a21b55423663952cc634909d6da9492cebb4
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-15 21:16:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def move(board, index, value)
   while counter < 9 && !over?(board)
     turn(board)
     if won?(board)
-       return "Congratulations #{winner(board)}!"
+       puts ("Congratulations #{winner(board)}!")
     elsif draw?(board)
       puts "Cat's Game!"
     end
